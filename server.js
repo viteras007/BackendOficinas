46f45813079d5e7075ee6056aa22a04c4aa4d1d1
Victor Hugo <viteras10@gmail.com>
2018-10-29 14:49:38 -0300

    atualizando o serviço para rodar 3001

--- server.js
+++ server.js
@@ -184,6 +184,6 @@ app.delete('/delfood', (req, res) => {
 
 
 //    ========================================== Servidor ==========================================
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(3001, () => {
+    console.log('app is running on port 3001');
 });
